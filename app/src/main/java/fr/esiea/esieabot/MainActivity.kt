410ba1713871d7b87fceea53c3b4fe0ad7848ba4
Nathan B <93329251+NathanB-ESIEA@users.noreply.github.com>
2021-12-01 11:17:21 +0100

    Amélioration de l'appli

--- app/src/main/java/fr/esiea/esieabot/MainActivity.kt
+++ app/src/main/java/fr/esiea/esieabot/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.esiea.esieabot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.esiea.esieabot.fragments.home_fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, home_fragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
