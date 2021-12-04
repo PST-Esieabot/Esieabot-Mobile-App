79400a693b10b728b108dc3aa5cda209d6a432a3
Nathan B <93329251+NathanB-ESIEA@users.noreply.github.com>
2021-12-04 14:41:40 +0100

    Update MainActivity.kt

--- app/src/main/java/fr/esiea/esieabot/MainActivity.kt
+++ app/src/main/java/fr/esiea/esieabot/MainActivity.kt
@@ -2,16 +2,44 @@ package fr.esiea.esieabot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fr.esiea.esieabot.fragments.home_fragment
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fr.esiea.esieabot.fragments.ControlFragment
+import fr.esiea.esieabot.fragments.HomeFragment
+import fr.esiea.esieabot.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Fragment
+
+        loadFragment(HomeFragment())
+
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation_view)
+        navigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.home_page -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.control_page -> {
+                    loadFragment(ControlFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.settings_page -> {
+                    loadFragment(SettingsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, home_fragment())
+        transaction.replace(R.id.fragment_container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
     }
