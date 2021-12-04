9d84bf936c48170196330853f564e1b3cf441987
Nathan B <93329251+NathanB-ESIEA@users.noreply.github.com>
2021-12-04 14:41:35 +0100

    Create HomeFragment.kt

---
+++ app/src/main/java/fr/esiea/esieabot/fragments/HomeFragment.kt
@@ -0,0 +1,15 @@
+package fr.esiea.esieabot.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.esiea.esieabot.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater?.inflate(R.layout.fragment_home, container, false)
+    }
+}
