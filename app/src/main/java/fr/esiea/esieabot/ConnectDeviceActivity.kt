cec3a15d323020e2c0b4880d9fd64d9e24a72c30
Nathan B <93329251+NathanB-ESIEA@users.noreply.github.com>
2021-12-20 11:35:54 +0100

    Ajout du numéro de version

---
+++ app/src/main/java/fr/esiea/esieabot/ConnectDeviceActivity.kt
@@ -0,0 +1,12 @@
+package fr.esiea.esieabot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConnectDeviceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_home)
+
+    }
+}
